5c3e07cd755f1f3328b425f97f5414d123b7a49c
Raimbault <daosuijun@outlook.com>
2025-03-13 12:42:54 +0800

    Update core.sh

--- core.sh
+++ core.sh
@@ -96,12 +96,10 @@ export REDIS_PASSWORD=$(openssl rand -hex 16)
 fetch -o "$BACKEND_SERVER_DIR/redis.conf" https://raw.githubusercontent.com/redis/redis/7.4/redis.conf
 
 # 更新配置文件
-sed -i '' 's/^port .*/port $REDIS_PORT\
-requirepass $REDIS_PASSWORD/' $BACKEND_SERVER_DIR/redis.conf
-sed -i '' 's/^appendonly yes$/appendonly no/' $BACKEND_SERVER_DIR/redis.conf
+sed -i '' "s/^port .*/port $REDIS_PORT/" $BACKEND_SERVER_DIR/redis.conf; sed -i '' -E "s/^# requirepass .*/requirepass $REDIS_PASSWORD/" $BACKEND_SERVER_DIR/redis.conf; sed -i '' 's/^appendonly yes$/appendonly no/' $BACKEND_SERVER_DIR/redis.conf
 
 # 启动 Redis
-screen -dmS redis_session redis-server $BACKEND_SERVER_DIR/redis.conf
+screen -dmS redis_session redis-server $BACKEND_SERVER_DIR/redis.conf; redis-cli -h 127.0.0.1 -p $REDIS_PORT -a $REDIS_PASSWORD ping
 
 # 创建 MongoDB 数据库
 OUT="$(
